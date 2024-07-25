9bbee690589c029e6ddb9f2d3c4a95a3cf233502
Milad Raeisi <miladsoft@yahoo.com>
2024-07-26 00:22:58 +0330

    Update Code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  site: 'https://sbc.om', // Your site URL
+  integrations: [react(), tailwind()],
+  site: 'https://sbc.om' 
 });
