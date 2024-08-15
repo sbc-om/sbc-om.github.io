eefbd929ee5d0967bafb7cb748a23384c9df4b5f
Milad Raeisi <miladsoft@yahoo.com>
2024-08-15 08:52:11 +0400

    Change the LOGO

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -34,6 +34,6 @@ export const OG = {
   type: "website",
   url: SITE.url,
   title: `${SITE.title}: : Hardware Tools & Construction Services`,
-  description: "Equip your projects with ScrewFast's top-quality hardware tools and expert construction services. Trusted by industry leaders, ScrewFast offers simplicity, affordability, and reliability. Experience the difference with user-centric design and cutting-edge tools. Start exploring now!",
+  description: "Equip your projects with Senior Blockchain's top-quality hardware tools and expert construction services. Trusted by industry leaders, Senior Blockchain offers simplicity, affordability, and reliability. Experience the difference with user-centric design and cutting-edge tools. Start exploring now!",
   image: ogImageSrc,
 };
